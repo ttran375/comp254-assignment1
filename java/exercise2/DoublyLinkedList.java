ad05d95f81e5259677a2cd662fe699163835a171
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-24 16:27:00 +0000

    Fri, May 24, 2024, 4:27 PM +00:00

--- java/exercise2/DoublyLinkedList.java
+++ java/exercise2/DoublyLinkedList.java
@@ -1,137 +1,127 @@
 package exercise2;
 
 public class DoublyLinkedList<E> {
-    private static class Node<E> {
-        private E element;
-        private Node<E> prev;
-        private Node<E> next;
 
-        public Node(E e, Node<E> p, Node<E> n) {
-            element = e;
-            prev = p;
-            next = n;
-        }
+  private static class Node<E> {
 
-        public E getElement() {
-            return element;
-        }
+    private E element;
+    private Node<E> prev;
+    private Node<E> next;
 
-        public Node<E> getPrev() {
-            return prev;
-        }
-
-        public Node<E> getNext() {
-            return next;
-        }
-
-        public void setPrev(Node<E> p) {
-            prev = p;
-        }
-
-        public void setNext(Node<E> n) {
-            next = n;
-        }
+    public Node(E e, Node<E> p, Node<E> n) {
+      element = e;
+      prev = p;
+      next = n;
     }
 
-    private Node<E> header;
-    private Node<E> trailer;
-    private int size = 0;
-
-    public DoublyLinkedList() {
-        header = new Node<>(null, null, null);
-        trailer = new Node<>(null, header, null);
-        header.setNext(trailer);
+    public E getElement() {
+      return element;
     }
 
-    public int size() {
-        return size;
+    public Node<E> getPrev() {
+      return prev;
     }
 
-    public boolean isEmpty() {
-        return size == 0;
+    public Node<E> getNext() {
+      return next;
     }
 
-    public E first() {
-        if (isEmpty())
-            return null;
-        return header.getNext().getElement();
+    public void setPrev(Node<E> p) {
+      prev = p;
     }
 
-    public E last() {
-        if (isEmpty())
-            return null;
-        return trailer.getPrev().getElement();
+    public void setNext(Node<E> n) {
+      next = n;
     }
 
-    public void addFirst(E e) {
-        addBetween(e, header, header.getNext());
-    }
+  }
 
-    public void addLast(E e) {
-        addBetween(e, trailer.getPrev(), trailer);
-    }
+  private Node<E> header;
+  private Node<E> trailer;
+  private int size = 0;
 
-    public E removeFirst() {
-        if (isEmpty())
-            return null;
-        return remove(header.getNext());
-    }
+  public DoublyLinkedList() {
+    header = new Node<>(null, null, null);
+    trailer = new Node<>(null, header, null);
+    header.setNext(trailer);
+  }
 
-    public E removeLast() {
-        if (isEmpty())
-            return null;
-        return remove(trailer.getPrev());
-    }
+  public int size() {
+    return size;
+  }
 
-    private void addBetween(E e, Node<E> predecessor, Node<E> successor) {
-        Node<E> newest = new Node<>(e, predecessor, successor);
-        predecessor.setNext(newest);
-        successor.setPrev(newest);
-        size++;
-    }
+  public boolean isEmpty() {
+    return size == 0;
+  }
 
-    private E remove(Node<E> node) {
-        Node<E> predecessor = node.getPrev();
-        Node<E> successor = node.getNext();
-        predecessor.setNext(successor);
-        successor.setPrev(predecessor);
-        size--;
-        return node.getElement();
-    }
+  public E first() {
+    if (isEmpty())
+      return null;
+    return header.getNext().getElement();
+  }
 
-    public String toString() {
-        StringBuilder sb = new StringBuilder("(");
-        Node<E> walk = header.getNext();
-        while (walk != trailer) {
-            sb.append(walk.getElement());
-            walk = walk.getNext();
-            if (walk != trailer)
-                sb.append(", ");
-        }
-        sb.append(")");
-        return sb.toString();
-    }
+  public E last() {
+    if (isEmpty())
+      return null;
+    return trailer.getPrev().getElement();
+  }
 
-    private void concatenate(DoublyLinkedList<String> list2) {
-        // TODO Auto-generated method stub
-        trailer.prev.setNext((Node<E>) list2.header.next);
-        list2.header.next.setPrev((Node<String>) trailer.getPrev());
-        trailer = (Node<E>) list2.trailer;
-    }
+  public void addFirst(E e) {
+    addBetween(e, header, header.getNext());
+  }
 
-    public static void main(String[] args) {
-        DoublyLinkedList<String> list1 = new DoublyLinkedList<String>();
-        list1.addFirst("MSP");
-        list1.addLast("ATL");
-        list1.addLast("BOS");
-        list1.addFirst("LAX");
-        System.out.println(list1);
+  public void addLast(E e) {
+    addBetween(e, trailer.getPrev(), trailer);
+  }
 
-        DoublyLinkedList<String> list2 = new DoublyLinkedList<String>();
-        list2.addFirst("YYZ");
-        list2.addLast("YVR");
-        System.out.println(list2);
-        list1.concatenate(list2);
-        System.out.println(list1);
+  public E removeFirst() {
+    if (isEmpty())
+      return null;
+    return remove(header.getNext());
+  }
+
+  public E removeLast() {
+    if (isEmpty())
+      return null;
+    return remove(trailer.getPrev());
+  }
+
+  private void addBetween(E e, Node<E> predecessor, Node<E> successor) {
+    Node<E> newest = new Node<>(e, predecessor, successor);
+    predecessor.setNext(newest);
+    successor.setPrev(newest);
+    size++;
+  }
+
+  private E remove(Node<E> node) {
+    Node<E> predecessor = node.getPrev();
+    Node<E> successor = node.getNext();
+    predecessor.setNext(successor);
+    successor.setPrev(predecessor);
+    size--;
+    return node.getElement();
+  }
+
+  public String toString() {
+    StringBuilder sb = new StringBuilder("(");
+    Node<E> walk = header.getNext();
+    while (walk != trailer) {
+      sb.append(walk.getElement());
+      walk = walk.getNext();
+      if (walk != trailer)
+        sb.append(", ");
     }
+    sb.append(")");
+    return sb.toString();
+  }
+
+  public static void main(String[] args) {
+    DoublyLinkedList<String> list = new DoublyLinkedList<String>();
+    list.addFirst("MSP");
+    list.addLast("ATL");
+    list.addLast("BOS");
+    list.addFirst("LAX");
+    System.out.println(list);
+    System.out.println(list.first());
+  }
 }
--- java/exercise2/SinglyLinkedList.java
+++ java/exercise2/SinglyLinkedList.java
@@ -1,168 +1,148 @@
 package exercise2;
 
 public class SinglyLinkedList<E> implements Cloneable {
+  private static class Node<E> {
+    private E element;
+    private Node<E> next;
 
-    private static class Node<E> {
-
-        private E element;
-        private Node<E> next;
-
-        public Node(E e, Node<E> n) {
-            element = e;
-            next = n;
-        }
-
-        public E getElement() {
-            return element;
-        }
-
-        public Node<E> getNext() {
-            return next;
-        }
-
-        public void setNext(Node<E> n) {
-            next = n;
-        }
+    public Node(E e, Node<E> n) {
+      element = e;
+      next = n;
     }
 
-    private Node<E> head = null;
-    private Node<E> tail = null;
-    private int size = 0;
-
-    public SinglyLinkedList() {
+    public E getElement() {
+      return element;
     }
 
-    public int size() {
-        return size;
+    public Node<E> getNext() {
+      return next;
     }
 
-    public boolean isEmpty() {
-        return size == 0;
+    public void setNext(Node<E> n) {
+      next = n;
     }
+  }
 
-    public E first() {
-        if (isEmpty())
-            return null;
-        return head.getElement();
+  private Node<E> head = null;
+  private Node<E> tail = null;
+  private int size = 0;
+
+  public SinglyLinkedList() {
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  public E first() {
+    if (isEmpty())
+      return null;
+    return head.getElement();
+  }
+
+  public E last() {
+    if (isEmpty())
+      return null;
+    return tail.getElement();
+  }
+
+  public void addFirst(E e) {
+    head = new Node<>(e, head);
+    if (size == 0)
+      tail = head;
+    size++;
+  }
+
+  public void addLast(E e) {
+    Node<E> newest = new Node<>(e, null);
+    if (isEmpty())
+      head = newest;
+    else
+      tail.setNext(newest);
+    tail = newest;
+    size++;
+  }
+
+  public E removeFirst() {
+    if (isEmpty())
+      return null;
+    E answer = head.getElement();
+    head = head.getNext();
+    size--;
+    if (size == 0)
+      tail = null;
+    return answer;
+  }
+
+  @SuppressWarnings({ "unchecked" })
+  public boolean equals(Object o) {
+    if (o == null)
+      return false;
+    if (getClass() != o.getClass())
+      return false;
+    SinglyLinkedList other = (SinglyLinkedList) o;
+    if (size != other.size)
+      return false;
+    Node walkA = head;
+    Node walkB = other.head;
+    while (walkA != null) {
+      if (!walkA.getElement().equals(walkB.getElement()))
+        return false;
+      walkA = walkA.getNext();
+      walkB = walkB.getNext();
     }
+    return true;
+  }
 
-    public E last() {
-        if (isEmpty())
-            return null;
-        return tail.getElement();
+  @SuppressWarnings({ "unchecked" })
+  public SinglyLinkedList<E> clone() throws CloneNotSupportedException {
+    SinglyLinkedList<E> other = (SinglyLinkedList<E>) super.clone();
+    if (size > 0) {
+      other.head = new Node<>(head.getElement(), null);
+      Node<E> walk = head.getNext();
+      Node<E> otherTail = other.head;
+      while (walk != null) {
+        Node<E> newest = new Node<>(walk.getElement(), null);
+        otherTail.setNext(newest);
+        otherTail = newest;
+        walk = walk.getNext();
+      }
     }
+    return other;
+  }
 
-    public void addFirst(E e) {
-        head = new Node<>(e, head);
-        if (size == 0)
-            tail = head;
-        size++;
+  public int hashCode() {
+    int h = 0;
+    for (Node walk = head; walk != null; walk = walk.getNext()) {
+      h ^= walk.getElement().hashCode();
+      h = (h << 5) | (h >>> 27);
     }
+    return h;
+  }
 
-    public void addLast(E e) {
-        Node<E> newest = new Node<>(e, null);
-        if (isEmpty())
-            head = newest;
-        else
-            tail.setNext(newest);
-        tail = newest;
-        size++;
+  public String toString() {
+    StringBuilder sb = new StringBuilder("(");
+    Node<E> walk = head;
+    while (walk != null) {
+      sb.append(walk.getElement());
+      if (walk != tail)
+        sb.append(", ");
+      walk = walk.getNext();
     }
+    sb.append(")");
+    return sb.toString();
+  }
 
-    public E removeFirst() {
-        if (isEmpty())
-            return null;
-        E answer = head.getElement();
-        head = head.getNext();
-        size--;
-        if (size == 0)
-            tail = null;
-        return answer;
-    }
-
-    @SuppressWarnings({ "unchecked" })
-    public boolean equals(Object o) {
-        if (o == null)
-            return false;
-        if (getClass() != o.getClass())
-            return false;
-        SinglyLinkedList other = (SinglyLinkedList) o;
-        if (size != other.size)
-            return false;
-        Node walkA = head;
-        Node walkB = other.head;
-        while (walkA != null) {
-            if (!walkA.getElement().equals(walkB.getElement()))
-                return false;
-            walkA = walkA.getNext();
-            walkB = walkB.getNext();
-        }
-        return true;
-    }
-
-    @SuppressWarnings({ "unchecked" })
-    public SinglyLinkedList<E> clone() throws CloneNotSupportedException {
-        SinglyLinkedList<E> other = (SinglyLinkedList<E>) super.clone();
-        if (size > 0) {
-            other.head = new Node<>(head.getElement(), null);
-            Node<E> walk = head.getNext();
-            Node<E> otherTail = other.head;
-            while (walk != null) {
-                Node<E> newest = new Node<>(walk.getElement(), null);
-                otherTail.setNext(newest);
-                otherTail = newest;
-                walk = walk.getNext();
-            }
-        }
-        return other;
-    }
-
-    public int hashCode() {
-        int h = 0;
-        for (Node walk = head; walk != null; walk = walk.getNext()) {
-            h ^= walk.getElement().hashCode();
-            h = (h << 5) | (h >>> 27);
-        }
-        return h;
-    }
-
-    public String toString() {
-        StringBuilder sb = new StringBuilder("(");
-        Node<E> walk = head;
-        while (walk != null) {
-            sb.append(walk.getElement());
-            if (walk != tail)
-                sb.append(", ");
-            walk = walk.getNext();
-        }
-        sb.append(")");
-        return sb.toString();
-    }
-
-    private void concatenate(SinglyLinkedList<String> list2) {
-        Node walk = head;
-        while (walk.next != null) {
-            walk = walk.getNext();
-        }
-        walk.setNext(list2.head);
-        size = this.size + list2.size;
-        tail = (Node<E>) list2.tail;
-    }
-
-    public static void main(String[] args) {
-        SinglyLinkedList<String> list1 = new SinglyLinkedList<String>();
-        list1.addFirst("MSP");
-        list1.addLast("ATL");
-        list1.addLast("BOS");
-        list1.addFirst("LAX");
-        System.out.println(list1);
-
-        SinglyLinkedList<String> list2 = new SinglyLinkedList<String>();
-        list2.addFirst("YYZ");
-        list2.addFirst("YVR");
-        System.out.println(list2);
-
-        list1.concatenate(list2);
-        System.out.println(list1);
-    }
+  public static void main(String[] args) {
+    SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+    list.addFirst("MSP");
+    list.addLast("ATL");
+    list.addLast("BOS");
+    list.addFirst("LAX");
+    System.out.println(list);
+  }
 }
