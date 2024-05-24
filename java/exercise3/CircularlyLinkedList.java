d5d11fc0ac047fcf3f01715f30ab6e41a9fa71be
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-24 18:37:48 +0000

    Fri, May 24, 2024, 6:37 PM +00:00

--- java/exercise3/CircularlyLinkedList.java
+++ java/exercise3/CircularlyLinkedList.java
@@ -1,63 +1,160 @@
 package exercise3;
 
-public class CircularlyLinkedList<E> {
-    private static class Node<E> {
-        private E element;
-        private Node<E> next;
+/*
+ * Copyright 2014, Michael T. Goodrich, Roberto Tamassia, Michael H. Goldwasser
+ *
+ * Developed for use with the book:
+ *
+ *    Data Structures and Algorithms in Java, Sixth Edition
+ *    Michael T. Goodrich, Roberto Tamassia, and Michael H. Goldwasser
+ *    John Wiley & Sons, 2014
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+/**
+ * An implementation of a circularly linked list.
+ *
+ * @author Michael T. Goodrich
+ * @author Roberto Tamassia
+ * @author Michael H. Goldwasser
+ */
+public class CircularlyLinkedList<E> {
+    // ---------------- nested Node class ----------------
+    /**
+     * Singly linked node, which stores a reference to its element and
+     * to the subsequent node in the list.
+     */
+    private static class Node<E> {
+
+        /** The element stored at this node */
+        private E element; // an element stored at this node
+
+        /** A reference to the subsequent node in the list */
+        private Node<E> next; // a reference to the subsequent node in the list
+
+        /**
+         * Creates a node with the given element and next node.
+         *
+         * @param e the element to be stored
+         * @param n reference to a node that should follow the new node
+         */
         public Node(E e, Node<E> n) {
             element = e;
             next = n;
         }
 
+        // Accessor methods
+        /**
+         * Returns the element stored at the node.
+         * 
+         * @return the element stored at the node
+         */
         public E getElement() {
             return element;
         }
 
+        /**
+         * Returns the node that follows this one (or null if no such node).
+         * 
+         * @return the following node
+         */
         public Node<E> getNext() {
             return next;
         }
 
+        // Modifier methods
+        /**
+         * Sets the node's next reference to point to Node n.
+         * 
+         * @param n the node that should follow this one
+         */
         public void setNext(Node<E> n) {
             next = n;
         }
-    }
+    } // ----------- end of nested Node class -----------
 
-    private Node<E> tail = null;
-    private int size = 0;
+    // instance variables of the CircularlyLinkedList
+    /** The designated cursor of the list */
+    private Node<E> tail = null; // we store tail (but not head)
 
+    /** Number of nodes in the list */
+    private int size = 0; // number of nodes in the list
+
+    /** Constructs an initially empty list. */
     public CircularlyLinkedList() {
-    }
+    } // constructs an initially empty list
 
+    // access methods
+    /**
+     * Returns the number of elements in the linked list.
+     * 
+     * @return number of elements in the linked list
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Tests whether the linked list is empty.
+     * 
+     * @return true if the linked list is empty, false otherwise
+     */
     public boolean isEmpty() {
         return size == 0;
     }
 
-    public E first() {
+    /**
+     * Returns (but does not remove) the first element of the list
+     * 
+     * @return element at the front of the list (or null if empty)
+     */
+    public E first() { // returns (but does not remove) the first element
         if (isEmpty())
             return null;
-        return tail.getNext().getElement();
+        return tail.getNext().getElement(); // the head is *after* the tail
     }
 
-    public E last() {
+    /**
+     * Returns (but does not remove) the last element of the list
+     * 
+     * @return element at the back of the list (or null if empty)
+     */
+    public E last() { // returns (but does not remove) the last element
         if (isEmpty())
             return null;
         return tail.getElement();
     }
 
-    public void rotate() {
-        if (tail != null)
-            tail = tail.getNext();
+    // update methods
+    /**
+     * Rotate the first element to the back of the list.
+     */
+    public void rotate() { // rotate the first element to the back of the list
+        if (tail != null) // if empty, do nothing
+            tail = tail.getNext(); // the old head becomes the new tail
     }
 
-    public void addFirst(E e) {
+    /**
+     * Adds an element to the front of the list.
+     * 
+     * @param e the new element to add
+     */
+    public void addFirst(E e) { // adds element e to the front of the list
         if (size == 0) {
             tail = new Node<>(e, null);
-            tail.setNext(tail);
+            tail.setNext(tail); // link to itself circularly
         } else {
             Node<E> newest = new Node<>(e, tail.getNext());
             tail.setNext(newest);
@@ -65,23 +162,37 @@ public class CircularlyLinkedList<E> {
         size++;
     }
 
-    public void addLast(E e) {
-        addFirst(e);
-        tail = tail.getNext();
+    /**
+     * Adds an element to the end of the list.
+     * 
+     * @param e the new element to add
+     */
+    public void addLast(E e) { // adds element e to the end of the list
+        addFirst(e); // insert new element at front of list
+        tail = tail.getNext(); // now new element becomes the tail
     }
 
-    public E removeFirst() {
+    /**
+     * Removes and returns the first element of the list.
+     * 
+     * @return the removed element (or null if empty)
+     */
+    public E removeFirst() { // removes and returns the first element
         if (isEmpty())
-            return null;
+            return null; // nothing to remove
         Node<E> head = tail.getNext();
         if (head == tail)
-            tail = null;
+            tail = null; // must be the only node left
         else
-            tail.setNext(head.getNext());
+            tail.setNext(head.getNext()); // removes "head" from the list
         size--;
         return head.getElement();
     }
 
+    /**
+     * Produces a string representation of the contents of the list.
+     * This exists for debugging purposes only.
+     */
     public String toString() {
         if (tail == null)
             return "()";
@@ -97,16 +208,33 @@ public class CircularlyLinkedList<E> {
         return sb.toString();
     }
 
+    // main method
     public static void main(String[] args) {
-        CircularlyLinkedList<String> circularList = new CircularlyLinkedList<String>();
-        circularList.addFirst("LAX");
-        circularList.addLast("MSP");
-        circularList.addLast("ATL");
-        circularList.addLast("BOS");
-        System.out.println(circularList);
-        circularList.removeFirst();
-        System.out.println(circularList);
-        circularList.rotate();
-        System.out.println(circularList);
+
+        // (LAX, MSP, ATL, BOS)
+        CircularlyLinkedList<String> circularlyList1 = new CircularlyLinkedList<String>();
+        circularlyList1.addFirst("LAX");
+        circularlyList1.addLast("MSP");
+        circularlyList1.addLast("ATL");
+        circularlyList1.addLast("BOS");
+        //
+        System.out.println(circularlyList1);
+        // circularlyList1.rotate();
+
+        CircularlyLinkedList<String> circularlyList2 = new CircularlyLinkedList<String>();
+        circularlyList2.addLast("YYZ");
+        circularlyList2.addLast("YVR");
+        System.out.println(circularlyList2);
+        circularlyList1.concatenate(circularlyList2);
+        System.out.println(circularlyList2);
+        //
+    }
+
+    private void concatenate(CircularlyLinkedList<String> circularlyList2) {
+        // TODO Auto-generated method stub
+        Node head = tail.getNext();
+        tail.setNext((Node<E>) circularlyList2.tail.getNext());
+        circularlyList2.tail.setNext(head);
+        tail = (Node<E>) circularlyList2.tail;
     }
 }
