21af878b21bd98e599b30709d1b54c4d62498cd8
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-24 16:23:47 +0000

    Fri, May 24, 2024, 4:23 PM +00:00

--- python/exercise1/SinglyLinkedlist.py
+++ python/exercise1/SinglyLinkedlist.py
@@ -116,5 +116,10 @@ if __name__ == "__main__":
     list1.add_first("MSP")
     list1.add_last("ATL")
     list1.add_last("BOS")
-    list1.remove_first()
-    print(list1)
+    list1.add_first("LAX")
+    print(f"Before swapping: {list1}")
+
+    node1 = list1.head.next_node
+    node2 = list1.tail
+    list1.swapTwoNodes(node1, node2)
+    print(f"After swapping: {list1}")
